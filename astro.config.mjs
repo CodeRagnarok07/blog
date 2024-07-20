4ca795a46a775da268bf7e5f199c5672c262c4a6
Angel riera <79159146+CodeRagnarok07@users.noreply.github.com>
2024-07-19 20:07:34 -0400

    deploy change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'CodeRagnarok Blog',
+			title: 'CodeRagnarok docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/CodeRagnarok07',
 				linkedin: 'https://www.linkedin.com/in/angelriera/',
 				email: 'mailto:angelriera1796@gmail.com?subject=with%20astro',
 			},
